06818ace72547edb8fb04f4e5509a39c21b24ac2
Saidnosir Burkhonov <saidnosirburxonov.com>
2026-03-14 13:47:40 +0500

    added

---
+++ Dars_4_1/Dars_4_1/AgeExsception.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dars_4_1;
+
+public class AgeExsception : Exception
+{
+    public AgeExsception(string message) : base(message) 
+    {
+        
+    }
+}
---
+++ Dars_4_1/Dars_4_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Dars_4_1;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        var age = 10;
+        try
+        {
+            if(age < 18)
+            {
+                throw new AgeExsception("yosh 18dan kichik");
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
